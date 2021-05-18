b1c49fe5203bf881c1a14e39aa20d112a78e996a
Joseph Riddle <joeriddles10@gmail.com>
2021-05-18 16:16:54 -0700

    Remove accidental copy

--- 2021.05.18-FastAPI copy/example-docker/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-
-app = FastAPI()
-
-
-@app.get('/docker/')
-def hello_docker():
-    return 'Hello Docker! 🐋'
--- 2021.05.18-FastAPI copy/example-security-token/users.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    username: str
-    token: str
