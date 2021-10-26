b0062a3535969772612fe6fcbb4e328849bb9a37
Joseph Riddle <joeriddles10@gmail.com>
2021-10-25 21:31:07 -0700

    [Pragmatic Python 1] Add BS4 example

--- 2021.11.04-Pragmatic-Python-1/app/app.py
+++ 2021.11.04-Pragmatic-Python-1/app/app.py
@@ -1,5 +1,7 @@
 import argparse
 
+import bs4
+from bs4.element import ResultSet, Tag
 import requests
 
 
@@ -12,6 +14,17 @@ def main():
     response = requests.get(url)
     if not response.ok:
         raise ValueError(f"url={url} returned status_code={response.status_code}")
+    
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    # print(soup.prettify())
+
+    results: ResultSet[Tag] = soup.find_all("li", class_="result-row") 
+    for result in results:
+        print(result["data-pid"])
+        result_title = result.select_one(".result-title")
+        if result_title is None:
+            raise ValueError("No element with .result-title found.")
+        print(result_title.text)
 
 
 if __name__ == "__main__":
