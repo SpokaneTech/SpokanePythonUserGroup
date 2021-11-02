2db0ad97d4a55f2adaa342e22e499e20fd2bcd6f
Joseph Riddle <joeriddles10@gmail.com>
2021-11-01 17:18:52 -0700

    Find lowest priced item

--- 2021.11.04-Pragmatic-Python-1/app/app.py
+++ 2021.11.04-Pragmatic-Python-1/app/app.py
@@ -1,13 +1,19 @@
 import argparse
+import re
 
 import bs4
 from bs4.element import ResultSet, Tag
 import requests
 
+from .items import Item
+
+
+items: list[Item] = []
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--url", "-U", required=True)
+    parser.add_argument("url")
     args = parser.parse_args()
 
     url: str = args.url
@@ -20,11 +26,29 @@ def main():
 
     results: ResultSet[Tag] = soup.find_all("li", class_="result-row") 
     for result in results:
-        print(result["data-pid"])
-        result_title = result.select_one(".result-title")
-        if result_title is None:
+        title_element = result.select_one(".result-title")
+        if title_element is None:
             raise ValueError("No element with .result-title found.")
-        print(result_title.text)
+
+        price_element = result.select_one(".result-price")
+        if price_element is None:
+            raise ValueError("No element with .result-price found.")
+
+        title = title_element.text
+
+        price_match = re.match(r"\$(\d+)", price_element.text)
+        if price_match is None:
+            raise ValueError(f"Could not parse result_price={price_element}")
+        price = int(price_match.groups()[0])
+
+        item = Item(title, price)
+        items.append(item)
+
+    item_prices = [item.price for item in items]
+    lowest_price = min(item_prices)
+    lowest_price_index = item_prices.index(lowest_price)
+    lowest_price_item = items[lowest_price_index]
+    print(f"{lowest_price_item.title} is the lowest priced vehicle at ${lowest_price}")
 
 
 if __name__ == "__main__":
---
+++ 2021.11.04-Pragmatic-Python-1/app/items.py
@@ -0,0 +1,5 @@
+
+class Item:
+    def __init__(self, title: str, price: int):
+        self.title = title
+        self.price = price
